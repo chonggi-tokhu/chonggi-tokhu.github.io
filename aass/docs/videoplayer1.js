12b4553c92bada7d83b5c74faa2a3dc5a790329c
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-03-11 13:39:53 +0900

    Update videoplayer1.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/docs/videoplayer1.js
+++ aass/docs/videoplayer1.js
@@ -1,5 +1,5 @@
 var $videoplayer1tag = $("videoplayer1");
-$videoplayer1tag.html(`<div><video width="400px" height="300px" src="https://chonggi-tokhu.github.io/merong.mp4" id="kkk" controls></video><span role="button" onclick="videoplayf1()"><svg height="200" width="200"><path d="M0 0 L50 50 L0 100 Z" fill="blue" /></svg></span><span role="button" onclick="videopausef1()"><svg height="200" width="200"><path d="M0 0 L10 0 L10 30 L0 30 Z" fill="orange" /></svg></span></div>`);
+$videoplayer1tag.html(`<div><video width="400px" height="300px" id="kkk"><source src="https://chonggi-tokhu.github.io/merong.mp4" type="video/mp4"></video><span role="button" onclick="videoplayf1()"><svg height="200" width="200"><path d="M0 0 L50 50 L0 100 Z" fill="blue" /></svg></span><span role="button" onclick="videopausef1()"><svg height="200" width="200"><path d="M0 0 L10 0 L10 30 L0 30 Z" fill="orange" /></svg></span></div>`);
 
 function videoplayf1() {
   document.getElementById("kkk").play();
