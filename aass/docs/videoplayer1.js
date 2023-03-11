1a0bc4cc7ca5eb82f719274586716dac47291f03
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-03-11 13:27:28 +0900

    Update videoplayer1.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/docs/videoplayer1.js
+++ aass/docs/videoplayer1.js
@@ -1,13 +1,11 @@
 var $videoplayer1tag = $("videoplayer1");
-var videosrc1 = $videoplayer1tag.getAttribute("data-video-src");
-var videoid1 = $videoplayer1tag.getAttribute("data-id");
-$videoplayer1tag.html(`<div><video width="400px" height="300px" src="` + videosrc1 + `" id="` + videoid1 + `"></video><span role="button" onclick="videoplayf1()"><svg height="200" width="200"><path d="M0 0 L50 50 L0 100 Z" fill="blue" /></svg></span><span role="button" onclick="videopausef1()"><svg height="200" width="200"><path d="M0 0 L10 0 L10 30 L0 30 Z" fill="orange" /></svg></span></div>`);
+$videoplayer1tag.html(`<div><video width="400px" height="300px" src="https://chonggi-tokhu.github.io/merong.mp4" id="kkk"></video><span role="button" onclick="videoplayf1()"><svg height="200" width="200"><path d="M0 0 L50 50 L0 100 Z" fill="blue" /></svg></span><span role="button" onclick="videopausef1()"><svg height="200" width="200"><path d="M0 0 L10 0 L10 30 L0 30 Z" fill="orange" /></svg></span></div>`);
 
 function videoplayf1() {
-  videoid1.play();
+  document.getElementById("kkk").play();
 }
 function videopausef1() {
-  videoid1.pause();
+  document.getElementById("kkk").pause();
 }
 
 
