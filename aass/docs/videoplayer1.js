3789cd524fa24cf62cff5c9fb1ca71e8816b7028
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-03-15 18:53:22 +0900

    Update videoplayer1.js
    시험용
    
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/docs/videoplayer1.js
+++ aass/docs/videoplayer1.js
@@ -9,4 +9,14 @@ function videopausef1() {
   document.getElementById("kkk").pause();
 }
 
+var videotagsss = $("videoplayer2");
+
+videotagsss.videotagmandulgi1 = function () {
+  var naeyong1 = `<video width="400" height="300" id="kkk" src=" ${videosrc1} "></video><span role="button" onclick="videoplayf1()"><svg height="200" width="200"><path d="M0 0 L50 50 L0 100 Z" fill="blue" /></svg></span><span role="button" onclick="videopausef1()"><svg height="200" width="200"><path d="M0 0 L10 0 L10 30 L0 30 Z" fill="orange" /></svg></span>`;
+  var videosrc1 = this.getAttribute("src");
+  this.innerHTML = naeyong1;
+};
+
+videotagsss.videotagmandulgi1();
+
 
