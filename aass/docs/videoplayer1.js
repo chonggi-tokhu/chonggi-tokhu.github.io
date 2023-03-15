59432c37a8032d865f3020fd9bda4efa46337665
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-03-15 19:20:09 +0900

    Update videoplayer1.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/docs/videoplayer1.js
+++ aass/docs/videoplayer1.js
@@ -3,10 +3,10 @@ var videosrc1 = document.getElementById("videoplayer1").getAttribute("src");
 $videoplayer1tag.html(`<video width="400" height="300" id="kkk" src=" ${videosrc1} "></video><span role="button" onclick="videoplayf1()"><svg height="200" width="200"><path d="M0 0 L50 50 L0 100 Z" fill="blue" /></svg></span><span role="button" onclick="videopausef1()"><svg height="200" width="200"><path d="M0 0 L10 0 L10 30 L0 30 Z" fill="orange" /></svg></span>`);
 
 function videoplayf1() {
-  document.getElementById("kkk").play();
+  videotagsss.getElementById("kkk").play();
 }
 function videopausef1() {
-  document.getElementById("kkk").pause();
+  videotagsss.pause();
 }
 
 var videotagsss = document.getElementsByTagName("videoplayer2");
