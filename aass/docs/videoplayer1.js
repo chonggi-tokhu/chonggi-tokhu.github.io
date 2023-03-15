32d85ff26cdaaa17598caeca48545385eb2d8a4d
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-03-15 19:25:30 +0900

    Update videoplayer1.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/docs/videoplayer1.js
+++ aass/docs/videoplayer1.js
@@ -2,21 +2,28 @@ var $videoplayer1tag = $("videoplayer1");
 var videosrc1 = document.getElementById("videoplayer1").getAttribute("src");
 $videoplayer1tag.html(`<video width="400" height="300" id="kkk" src=" ${videosrc1} "></video><span role="button" onclick="videoplayf1()"><svg height="200" width="200"><path d="M0 0 L50 50 L0 100 Z" fill="blue" /></svg></span><span role="button" onclick="videopausef1()"><svg height="200" width="200"><path d="M0 0 L10 0 L10 30 L0 30 Z" fill="orange" /></svg></span>`);
 
-function videoplayf1() {
-  videotagsss.play();
+videotagsss.videoplayf1 = function (kkk) {
+  kkk.play();
 }
-function videopausef1() {
-  videotagsss.pause();
+videotagsss.videopausef1 = function (kkk) {
+  kkk.pause();
 }
 
 var videotagsss = document.getElementsByTagName("videoplayer2");
 
 videotagsss.videotagmandulgi1 = function (kkk) {
   var videosrc1 = kkk.getAttribute("src");
-  var naeyong1 = `<video width="400" height="300" src=" ${videosrc1} "></video><span role="button" onclick="videoplayf1()"><svg height="200" width="200"><path d="M0 0 L50 50 L0 100 Z" fill="blue" /></svg></span><span role="button" onclick="videopausef1()"><svg height="200" width="200"><path d="M0 0 L10 0 L10 30 L0 30 Z" fill="orange" /></svg></span>`;
+  var naeyong1 = `<video width="400" height="300" src=" ${videosrc1} "></video><span role="button" id="videoplayf1"><svg height="200" width="200"><path d="M0 0 L50 50 L0 100 Z" fill="blue" /></svg></span><span role="button" id="videopausef1"><svg height="200" width="200"><path d="M0 0 L10 0 L10 30 L0 30 Z" fill="orange" /></svg></span>`;
   kkk.innerHTML = naeyong1;
 };
 
 videotagsss.videotagmandulgi1(this);
+document.getElementById("videoplayf1").onclick = function () {
+    videotagsss.videoplayf1(this);
+}
+
+document.getElementById("videopausef1").onclick = function () {
+    videotagsss.videopausef1(this);
+}
 
 
