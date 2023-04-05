b201438805383c79228d5900ece981641b262b9c
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-04-05 14:41:33 +0900

    Update icon.css-1-0.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/docs/colourgrey-styles-1.0/js/icon.css-1-0.js
+++ aass/docs/colourgrey-styles-1.0/js/icon.css-1-0.js
@@ -1,12 +1,12 @@
 function myicofunction1() {
-var icoxxx1 = document.getElementsByTagName("cgreyico1");
-for (var i = 0; i < icoxxx1.length; i++) {
+  var icoxxx1 = document.getElementsByTagName("cgreyico1");
+  for (var i = 0; i < icoxxx1.length; i++) {
     var icoxxx2 = icoxxx1[i];
-    var icoxsrc1 = icoxxx2.getAttribute("type");
-    var naeyong5 = `<span style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/ ${icoxsrc1} );height:20px;width:20px;"></span>`;
-    icoxxx2.innerHTML = naeyong5;
+    var icoxsrc1 = icoxxx1[i].getAttribute("type");
+    var naeyong5 = `<span style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/${icoxsrc1});height:20px;width:20px;"></span>`;
+    icoxxx1[i].innerHTML = naeyong5;
+  }
 }
-}
-window.onload = function() {
-myicofunction1();
+window.onload = function () {
+  myicofunction1();
 }
