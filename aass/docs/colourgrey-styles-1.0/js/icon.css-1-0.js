5a3428ab0ac76040bd951e7856785c2890fa575f
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-04-05 14:57:50 +0900

    Update icon.css-1-0.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/docs/colourgrey-styles-1.0/js/icon.css-1-0.js
+++ aass/docs/colourgrey-styles-1.0/js/icon.css-1-0.js
@@ -3,7 +3,7 @@ function myicofunction1() {
   for (var i = 0; i < icoxxx1.length; i++) {
     var icoxxx2 = icoxxx1[i];
     var icoxsrc1 = icoxxx1[i].getAttribute("type");
-    var naeyong5 = `<i style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/${icoxsrc1});height:20px;width:20px;"></i>`;
+    var naeyong5 = `<span style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/${icoxsrc1});height:20px;width:20px;display:block;">_</span>`;
     icoxxx1[i].innerHTML = naeyong5;
   }
 }
