eb52e409388bebe168f3f275979868c773e24e51
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-04-05 13:18:51 +0900

    Create icon.css-1-0.js
    iconcssJs
    
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

---
+++ aass/docs/colourgrey-styles-1.0/js/icon.css-1-0.js
@@ -0,0 +1,6 @@
+var icoxxx1 = document.getElementsByTagName("cgreyico1");
+for (var i1 = 0; i1 < icoxxx1.length; i1++) {
+    var icoxxx2 = icoxxx1[i1];
+    var icoxsrc1 = icoxxx2.getAttribute("type");
+    icoxxx2.innerHTML = `<span style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/ ${icoxsrc1} );height:20px;width:20px;"></span>`
+}
