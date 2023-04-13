b7786ede0e4a21cf4f44238a9e2b3d72fbfe0a37
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-04-13 17:57:58 +0900

    Update blog.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/lfbs/blog.js
+++ aass/lfbs/blog.js
@@ -1,19 +1,19 @@
 function documentfunction1(tf) {
     var documentstring1 = document.body.innerHTML;
     if (tf) {
-    var abc1 = documentstring1.replace(':lol',`<span style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/lol.gif);height:20px;width:20px;display:block;"></span>`);
-    abc1 = documentstring1.replace(':heart', '\u2764');
-    abc1 = documentstring1.replace('chess.com', `<a href="https://www.chess.com">chess.com</a>`);
-    abc1 = documentstring1.replace('colourgrey', `<a href="https://www.chess.com/member/colourgrey">colourgrey</a>`);
-    abc1 = documentstring1.replace('colourgreyBOT', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
-    abc1 = documentstring1.replace('컬그봇', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
-    abc1 = documentstring1.replace('컬러그레이봇', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
-    abc1 = documentstring1.replace('savemartin', `<a href="https://www.chess.com/club/savemartin">savemartin</a>`);
-    abc1 = documentstring1.replace('SaveMartin', `<a href="https://www.chess.com/club/savemartin">SaveMartin</a>`);
-    abc1 = documentstring1.replace('#savemartin', `<a href="https://www.chess.com/club/savemartin">#savemartin</a>`);
-    abc1 = documentstring1.replace('세이브마틴', `<a href="https://www.chess.com/club/savemartin">세이브마틴</a>`);
-    abc1 = documentstring1.replace('#세이브마틴', `<a href="https://www.chess.com/club/savemartin">#세이브마틴</a>`);
-    return abc1;
+    var abc10 = documentstring1.replace(':lol',`<span style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/lol.gif);height:20px;width:20px;display:block;"></span>`);
+    var abc11 = abc10.replace(':heart', '\u2764');
+    var abc12 = abc11.replace('chess.com', `<a href="https://www.chess.com">chess.com</a>`);
+    var abc13 = abc12.replace('colourgrey', `<a href="https://www.chess.com/member/colourgrey">colourgrey</a>`);
+    var abc14 = abc13.replace('colourgreyBOT', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
+    var abc15 = abc14.replace('컬그봇', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
+    var abc16 = abc15.replace('컬러그레이봇', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
+    var abc17 = abc16.replace('savemartin', `<a href="https://www.chess.com/club/savemartin">savemartin</a>`);
+    var abc18 = abc17.replace('SaveMartin', `<a href="https://www.chess.com/club/savemartin">SaveMartin</a>`);
+    var abc19 = abc18.documentstring1.replace('#savemartin', `<a href="https://www.chess.com/club/savemartin">#savemartin</a>`);
+    var abc20 = abc18.replace('세이브마틴', `<a href="https://www.chess.com/club/savemartin">세이브마틴</a>`);
+    var abc21 = abc20.replace('#세이브마틴', `<a href="https://www.chess.com/club/savemartin">#세이브마틴</a>`);
+    return abc21;
     }
 }
 document.body.innerHTML = documentfunction1(true);
