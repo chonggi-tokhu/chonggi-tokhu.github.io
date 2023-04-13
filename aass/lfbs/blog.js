ba82389591eddb5fb480ab3d3fc89f89b27d61a0
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-04-13 17:32:09 +0900

    Update blog.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/lfbs/blog.js
+++ aass/lfbs/blog.js
@@ -1,7 +1,7 @@
 function documentfunction1() {
     var documentstring1 = document.body.innerHTML;
-    documentstring1.replace(`:lol`,`<span style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/lol.gif);height:20px;width:20px;display:block;"></span>`);
-    documentstring1.replace(`:heart`, '\u2764');
+    documentstring1.replace(':lol',`<span style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/lol.gif);height:20px;width:20px;display:block;"></span>`);
+    documentstring1.replace(':heart', '\u2764');
     function swearfiltering(swearfiltering1) {
         if (swearfiltering1) {
             documentstring1.replace('fuck', `oh,it's f-word.`);
@@ -12,16 +12,16 @@ function documentfunction1() {
             documentstring1.replace('nigger', `i'm racist`);
         }
     }
-    documentstring1.replace(`chess.com`, `<a href="https://www.chess.com">chess.com</a>`);
-    documentstring1.replace(`colourgrey`, `<a href="https://www.chess.com/member/colourgrey">colourgrey</a>`);
-    documentstring1.replace(`colourgreyBOT`, `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
-    documentstring1.replace(`컬그봇`, `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
-    documentstring1.replace(`컬러그레이봇`, `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
-    documentstring1.replace(`savemartin`, `<a href="https://www.chess.com/club/savemartin">savemartin</a>`);
-    documentstring1.replace(`SaveMartin`, `<a href="https://www.chess.com/club/savemartin">SaveMartin</a>`);
-    documentstring1.replace(`#savemartin`, `<a href="https://www.chess.com/club/savemartin">#savemartin</a>`);
-    documentstring1.replace(`세이브마틴`, `<a href="https://www.chess.com/club/savemartin">세이브마틴</a>`);
-    documentstring1.replace(`#세이브마틴`, `<a href="https://www.chess.com/club/savemartin">#세이브마틴</a>`);
+    documentstring1.replace('chess.com', `<a href="https://www.chess.com">chess.com</a>`);
+    documentstring1.replace('colourgrey', `<a href="https://www.chess.com/member/colourgrey">colourgrey</a>`);
+    documentstring1.replace('colourgreyBOT', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
+    documentstring1.replace('컬그봇', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
+    documentstring1.replace('컬러그레이봇', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
+    documentstring1.replace('savemartin', `<a href="https://www.chess.com/club/savemartin">savemartin</a>`);
+    documentstring1.replace('SaveMartin', `<a href="https://www.chess.com/club/savemartin">SaveMartin</a>`);
+    documentstring1.replace('#savemartin', `<a href="https://www.chess.com/club/savemartin">#savemartin</a>`);
+    documentstring1.replace('세이브마틴', `<a href="https://www.chess.com/club/savemartin">세이브마틴</a>`);
+    documentstring1.replace('#세이브마틴', `<a href="https://www.chess.com/club/savemartin">#세이브마틴</a>`);
     return documentstring1;
 }
 document.body.innerHTML = documentfunction1();
