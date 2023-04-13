ae982086b1ecec39966eacea113888d12043ca5c
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-04-13 17:51:36 +0900

    Update blog.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/lfbs/blog.js
+++ aass/lfbs/blog.js
@@ -2,7 +2,7 @@ function documentfunction1(tf) {
     var documentstring1 = document.body.innerHTML;
     if (tf) {
     var abc1 = documentstring1.replace(':lol',`<span style="background-image:url(https://chonggi-tokhu.github.io/aass/docs/colourgrey-styles-1.0/lol.gif);height:20px;width:20px;display:block;"></span>`);
-    var abc = documentstring1.replace(':heart', '\u2764');
+    var abc1 = documentstring1.replace(':heart', '\u2764');
     var abc1 = documentstring1.replace('chess.com', `<a href="https://www.chess.com">chess.com</a>`);
     var abc1 = documentstring1.replace('colourgrey', `<a href="https://www.chess.com/member/colourgrey">colourgrey</a>`);
     var abc1 = documentstring1.replace('colourgreyBOT', `<a href="https://chonggi-tokhu.github.io/aass/docs/chess/colourgreybot1">colourgrey-BOT</a>`);
@@ -13,7 +13,7 @@ function documentfunction1(tf) {
     var abc1 = documentstring1.replace('#savemartin', `<a href="https://www.chess.com/club/savemartin">#savemartin</a>`);
     var abc1 = documentstring1.replace('세이브마틴', `<a href="https://www.chess.com/club/savemartin">세이브마틴</a>`);
     var abc1 = documentstring1.replace('#세이브마틴', `<a href="https://www.chess.com/club/savemartin">#세이브마틴</a>`);
-    return abc;
+    return abc1;
     }
 }
 document.body.innerHTML = documentfunction1(true);
