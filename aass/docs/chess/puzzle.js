472513a9b7d21a3363cf39d83b9392776fea38a0
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-05-16 17:03:05 +0900

    Update puzzle.js
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

--- aass/docs/chess/puzzle.js
+++ aass/docs/chess/puzzle.js
@@ -9,6 +9,7 @@ class mypuzzlesdata {
           move: ["Qf4", "Bd4#"],
           colour: "w",
           level: 2,
+          source: `from <a href="https://lichess.org/@/baekhokp9027">colourgrey</a>'s game`,
         },
         {
           position: "r4k1r/ppp2ppp/8/q7/8/8/PPP1QPPP/4R1K1 w - - 0 1",
@@ -16,9 +17,45 @@ class mypuzzlesdata {
           move: ["Qe7+", "Qe8+", "Rxe8#"],
           colour: "w",
           level: 1,
+          source: `creation by <a href="https://www.chess.com/member/colourgrey">colourgrey</a>`,
         },
       ],
       opening_ruylopez: [{}],
+      opening_scotchgambit: [{}],
+      greek_gift: [],
+      allthemes: [
+        {
+          position: "r4rk1/1p4pp/p2p4/1b1P2q1/3NP3/1P1nR2Q/7P/7K b - - 6 28",
+          oppmove: [],
+          move: ["Nf2#"],
+          level: 4,
+          source: `from <a href="https://www.chess.com/member/colourgrey">colourgrey</a>'s <a href="https://www.chess.com/game/live/70188787003">game</a>`,
+        },
+        {
+          position:
+            "r1bq1rk1/pp3ppp/2nbp3/2pp3n/3P1B2/1PPBPN2/P2N1PPP/R2QK2R w KQ - 3 9",
+          oppmove: ["Kxh7", "Kg8", "g6"],
+          move: ["Bxh7+", "Ng5+", "Qxh5", "Qh7#"],
+          level: 3,
+          source: `creation by <a href="https://www.chess.com/member/colourgrey">colourgrey</a>`,
+        },
+        {
+          position: "r6k/ppp1n2p/3pB1p1/4pP2/8/3PBQ2/PPP2KP1/7N w - - 0 18",
+          oppmove: ["exf4"],
+          move: ["Qf4", "Bd4#"],
+          colour: "w",
+          level: 2,
+          source: `from <a href="https://lichess.org/@/baekhokp9027">colourgrey</a>'s game`,
+        },
+        {
+          position: "r4k1r/ppp2ppp/8/q7/8/8/PPP1QPPP/4R1K1 w - - 0 1",
+          oppmove: ["Kg8", "Rxe8"],
+          move: ["Qe7+", "Qe8+", "Rxe8#"],
+          colour: "w",
+          level: 1,
+          source: `creation by <a href="https://www.chess.com/member/colourgrey">colourgrey</a>`,
+        },
+      ],
     };
   }
   typepuzzle1() {
