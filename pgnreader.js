2b04c80d435d75ce3332158aff0547a5a1bff258
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-07-29 00:25:07 +0900

    Add files via upload
    Signed-off-by: chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>

---
+++ pgnreader.js
@@ -0,0 +1,133 @@
+function colourgreyPGN() {}
+
+colourgreyPGN.prototype = {
+  gameheader1(pgnstring) {
+    var pgnstring1 = pgnstring;
+    var pgnheader1 = {};
+    var pgnstring2 = pgnstring1.split(`[`);
+    for (var i02k9 = 0; i02k9 < pgnstring2.length; i02k9++) {
+      var thtv1 = pgnstring2[i02k9];
+      var thtv2 = {
+        desc: thtv1.split(`"`)[0].replace(`]`, ``),
+        v0: thtv1.split(`"`)[1],
+      };
+      pgnheader1[thtv2.desc] = thtv2.v0;
+      if (pgnheader1 == undefined || pgnheader1 == {}) {
+        continue;
+      } else if (i02k9 == pgnstring2.length - 1) {
+        return pgnheader1;
+      }
+    }
+  },
+  gamehistory1(pgnstring) {
+    var cgf1 = pgnstring.split(`]`)[pgnstring.split(`]`).length - 1];
+    console.log(cgf1);
+    var kakkakmoves1 = [];
+    var spacechar = " ";
+    var fullstopchar = ".";
+    var ojk = 0;
+    var chae1;
+    var cgf2 = cgf1;
+    for (var ijx = 0; ijx < cgf1.length; ijx++) {
+      cgf1 = cgf1.replace("0.", "");
+      cgf1 = cgf1.replace("1.", "");
+      cgf1 = cgf1.replace("2.", "");
+      cgf1 = cgf1.replace("3.", "");
+      cgf1 = cgf1.replace("4.", "");
+      cgf1 = cgf1.replace("5.", "");
+      cgf1 = cgf1.replace("6.", "");
+      cgf1 = cgf1.replace("7.", "");
+      cgf1 = cgf1.replace("8.", "");
+      cgf1 = cgf1.replace("9.", "");
+      cgf1 = cgf1.replace(" 0", "");
+      cgf1 = cgf1.replace(" 1", "");
+      cgf1 = cgf1.replace(" 2", "");
+      cgf1 = cgf1.replace(" 3", "");
+      cgf1 = cgf1.replace(" 4", "");
+      cgf1 = cgf1.replace(" 5", "");
+      cgf1 = cgf1.replace(" 6", "");
+      cgf1 = cgf1.replace(" 7", "");
+      cgf1 = cgf1.replace(" 8", "");
+      cgf1 = cgf1.replace(" 9", "");
+      cgf1 = cgf1.replace("-1", " 0-1");
+      cgf1 = cgf1.replace("-0", " 1-0");
+      cgf1 = cgf1.replace("  ", " ");
+      console.log(cgf1);
+      if (ijx == cgf1.length - 1) {
+        for (var i = 0; i < cgf1.length; i++) {
+          if (cgf1[i] == spacechar) {
+            var avv = true;
+            var sijak123 = i;
+            for (var n = i + 1; n < cgf1.length; n++) {
+              if (cgf1[n] == " ") {
+                var kkut123 = n;
+
+                kakkakmoves1[ojk] = cgf1.slice(i + 1, n);
+                console.log(kakkakmoves1[ojk]);
+                ojk = ojk + 1;
+                break;
+              }
+            }
+          }
+          if (kakkakmoves1 == []) {
+            continue;
+          } else if (i == cgf1.length - 1) {
+            return kakkakmoves1;
+          }
+        }
+      } else {
+        continue;
+      }
+    }
+  },
+  gamehistory2(pgnstring12) {
+    var abcsss = this.gamehistory1(pgnstring12);
+    for (var i8sss = 0; i8sss < abcsss.length; i8sss++) {
+      if (abcsss == undefined) {
+        continue;
+      } else if (i8sss == abcsss.length - 1) {
+        return abcsss;
+      }
+    }
+  },
+  rtparsedpgn(pgnstring5) {
+    return {
+      headerofgame: this.gameheader1(pgnstring5),
+      gamehistorywr: this.gamehistory1(pgnstring5),
+    };
+  },
+};
+
+var parsingpgn = new colourgreyPGN();
+
+var wallyex = `[Event "vs Computer"]
+[Site "Chess.com"]
+[Date "2023.07.28"]
+[Round "-"]
+[White "Wally-BOT"]
+[Black "lemontreesong0101"]
+[Result "0-1"]
+[CurrentPosition "2kr3r/pp2bp2/n5pp/1N5K/Q5P1/P4q2/5P1P/R6R w - - 0 27"]
+[Timezone "UTC"]
+[ECO "D50"]
+[ECOUrl "https://www.chess.com/openings/Queens-Gambit-Declined-Modern-Variation"]
+[UTCDate "2023.07.28"]
+[UTCTime "04:54:38"]
+[WhiteElo "1800"]
+[BlackElo "706"]
+[TimeControl "1/0"]
+[Termination "lemontreesong0101 won by checkmate"]
+[StartTime "04:54:38"]
+[EndDate "2023.07.28"]
+[EndTime "05:01:11"]
+[Link "https://www.chess.com/game/computer/79939671"]
+[WhiteUrl "https://images.chesscomfiles.com/uploads/v1/user/66746018.e647839e.50x50o.503c26216762.png"]
+[WhiteCountry "162"]
+[WhiteTitle ""]
+[BlackUrl "https://images.chesscomfiles.com/uploads/v1/user/298618469.75a42c5b.50x50o.7cce829f2a99.jpg"]
+[BlackCountry "164"]
+[BlackTitle ""]
+
+1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. Bg5 h6 5. Bxf6 Qxf6 6. cxd5 exd5 7. Nxd5 Qd8 8. e4 c6 9. Nc3 Be6 10. d5 cxd5 11. exd5 Bd7 12. d6 Nc6 13. Bc4 Qf6 14. Nb5 O-O-O 15. Kf1 Nb4 16. a3 Na6 17. Be6 Bxe6 18. Ne2 Bc4 19. Qa4 Bxe2+ 20. Kxe2 Qxb2+ 21. Kf3 Qf6+ 22. Kg3 Bxd6+ 23. Kh3 Qf5+ 24. g4 Qf3+ 25. Kh4 Be7+ 26. Kh5 g6# 0-1`;
+
+var wallyex2 = parsingpgn.rtparsedpgn(wallyex);
